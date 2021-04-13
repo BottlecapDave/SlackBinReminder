319ed9d39bf78b8c94acfbf24ec6faaeaa73ab6c
BottlecapDave <apphub@davidskendall.co.uk>
2021-04-13 07:15:47 +0100

    Updated build command to build lambda function

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-dotnet build ./src/SlackBinReminder -c Release -o out
-dotnet publish ./src/SlackBinReminder -c Release -o out /p:GenerateRuntimeConfigurationFiles=true
+dotnet build ./src/SlackBinReminder/SlackBinReminder.Lambda -c Release -o out
+dotnet publish ./src/SlackBinReminder/SlackBinReminder.Lambda -c Release -o out /p:GenerateRuntimeConfigurationFiles=true
